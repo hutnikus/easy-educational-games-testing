71c56b52d08292458a9b5359c4a2615142c0bd8d
hutnikus <59333824+hutnikus@users.noreply.github.com>
2022-01-15 14:37:27 +0100

    added new test functions

--- public/game.js
+++ public/game.js
@@ -1,4 +1,4 @@
-import {Game, GameElement, GameShape, GameText, Point} from "../modules/index.js"
+import {Game, GameElement, GameShape, GameText, GameImage, GameGif, Point} from "../modules/index.js"
 
 const canvas = document.getElementById('game');
 canvas.width = 600;
@@ -6,34 +6,42 @@ canvas.height = 600;
 
 const game = new Game(canvas);
 
+// test code to check if area is inside an element
+// canvas.addEventListener('mousemove',async (event) => await game.drawInside(event))
+
+// satellites that orbit each other and draw their path on canvas
 function inspiro() {
-    //point rotating around mouse
-    let drawn, circles, angles, radii
 
-    function init() {
-        drawn = new GameElement(0, 0, [], {clickable: false})
-        game.addElement(drawn)
-
-        circles = [new GameElement(300, 300,
-            [new GameShape('oval', {rX: 2, rY: 2, fill: 'random', level: 1})],
-            {clickable: false, name: '0'}
-        )]
-        game.addElement(circles[0])
-        angles = [startAngle]
-        radii = [64]
-
-        for (let i = 0; i < satellites-1; i++) {
-            addAnchor()
-        }
-    }
-
+    // center
     let lastPos = Point(300,300)
     let drawing = true
 
     const petals = 6
-    const satellites = 3
-    const angleDif = 4/5
+    const satellites = 4
+    const angleDif = 5/6
     const startAngle = -Math.PI/2
+    const startRadius = 64
+
+    const lineWidth = 2
+    const circleRadius = 4
+
+    let circles, drawn, angles, radii
+
+    function init() {
+        // points rotating around each other
+        circles = []
+        // element that draws the shapes
+        drawn = new GameElement(0, 0, [], {clickable: false})
+        game.addElement(drawn)
+        // current angles of points relative to each other
+        angles = []
+        // distance of rotating circles to each other
+        radii = []
+
+        for (let i = 0; i < satellites; i++) {
+            addAnchor()
+        }
+    }
 
     init()
 
@@ -45,19 +53,49 @@ function inspiro() {
     }
 
     function addAnchor() {
-        const lastCircle = circles[circles.length-1]
-        const newR = radii[radii.length-1] * angleDif
-        radii.push(newR)
-        angles.push(startAngle)
-        circles.push(
-            new GameElement(
-                newR * Math.cos(0) + lastCircle.center.x,
-                newR * Math.sin(0) + lastCircle.center.y,
-                [new GameShape('oval',{rX:2,rY:2,fill:'random',level:1})],
+        let pos, newR
+
+        let lastCircle
+        if (circles.length > 0) {
+            // pridavam
+            pos = circles[circles.length-1].center
+            newR = radii[radii.length-1] * angleDif
+
+            lastCircle = new GameElement(
+                newR * Math.cos(0) + pos.x,
+                newR * Math.sin(0) + pos.y,
+                [new GameShape('oval',{rX:circleRadius,rY:circleRadius,fill:'random',level:1})],
                 {clickable:false,name:`${circles.length}`}
             )
+
+            circles.push(lastCircle)
+
+            rotateAround(lastCircle,pos,{r:newR,angle:startAngle})
+        } else {
+            // vytvaram prvy
+            newR = startRadius
+            pos = Point(
+                newR * Math.cos(startAngle) + 300,
+                newR * Math.sin(startAngle) + 300
+            )
+
+            lastCircle = new GameElement(
+                pos.x,
+                pos.y,
+                [new GameShape('oval',{rX:circleRadius,rY:circleRadius,fill:'random',level:1})],
+                {clickable:false,name:`${circles.length}`}
+            )
+
+            circles.push(lastCircle)
+        }
+
+        radii.push(newR)
+        angles.push(startAngle)
+
+        game.addElement(lastCircle)
+        drawn.addChild(
+            new GameShape('line',{coords:[lastCircle.center.x,lastCircle.center.y,lastCircle.center.x,lastCircle.center.y],stroke:lastCircle.children[0].fill,lineWidth:lineWidth,})
         )
-        game.addElement(circles[circles.length-1])
     }
 
     function keypress(event) {
@@ -69,9 +107,12 @@ function inspiro() {
             game.clear()
             init()
         }
+        if (event.key === '+') {
+            next(drawing)
+        }
     }
 
-    function tick(isDrawing) {
+    function next(isDrawing) {
         for (const i in circles) {
             angles[i] += 0.01 + ((petals/100)*i)
 
@@ -85,21 +126,23 @@ function inspiro() {
             rotateAround(circles[i],pos,{r:radii[i],angle:angles[i]})
 
             if (isDrawing) {
-                const drawingCircle = circles[i].children[0]
-                const newChild = new GameShape('oval',{rX:2,rY:2,fill:drawingCircle.fill,dx:circles[i].center.x,dy:circles[i].center.y,level:1})
-                drawn.addChild(newChild,false)
+                const drawingLine = drawn.children[i]
+                drawingLine.addPoint(Point(circles[i].center.x,circles[i].center.y))
             }
         }
+    }
 
-        setTimeout(()=>tick(drawing),5)
+    function tick(isDrawing) {
+        next(isDrawing)
+        setTimeout(()=>tick(drawing),0)
     }
     tick(drawing)
 
     document.addEventListener('keypress',(ev => keypress(ev)))
 }
-inspiro()
+// inspiro()
 
-// let's try creating a drawing function
+// test drawing function
 function drawing() {
     let drawElement = undefined;
     let initialPos = undefined;
@@ -143,7 +186,7 @@ function drawing() {
 }
 // drawing()
 
-// let's try creating a drag and drop function
+// test drag and drop function (you need to create some elements for this - try uncommenting function call pogs())
 function dragDrop() {
     let draggedElement = undefined;
     let delta = undefined;
@@ -180,12 +223,9 @@ function dragDrop() {
     canvas.addEventListener('mousemove',(ev => continueDragging(ev)))
     canvas.addEventListener('mouseup',(ev => finishDragging(ev)))
 }
-// dragDrop()
+dragDrop()
 
-// test code to check if clicked inside an element
-// canvas.addEventListener('mousemove',async (event) => await game.drawInside(event))
-
-// let's try creating a button that outputs a text to console
+// test button that outputs a text to console
 function button() {
     const button = new GameElement(300,300,
         [
@@ -199,20 +239,122 @@ function button() {
 }
 // button()
 
+// colorful circle objects to test clicking or drawing
+function pogs() {
+    const element1 = new GameElement(250, 250,
+        [
+            new GameText('1', {level: 2}),
+            new GameShape('oval', {
+                rX: 100,
+                rY: 100,
+                fill: 'red',
+                level: 1,
+                rotation: 0.2,
+                stroke: 'black',
+                lineWidth: 20
+            }),
+        ],
+        {clickable: true, name: '1-red', level: 10}
+    )
+    game.addElement(element1)
+
+    const element2 = new GameElement(350, 250,
+        [
+            new GameText('2', {level: 10}),
+            new GameShape('oval', {
+                rX: 100,
+                rY: 100,
+                fill: 'blue',
+                level: 1,
+                rotation: 0.2,
+                stroke: 'black',
+                lineWidth: 20
+            }),
+        ],
+        {clickable: true, name: '2-blue'}
+    )
+    game.addElement(element2)
+
+    const element3 = new GameElement(250, 350,
+        [
+            new GameText('3', {level: 10}),
+            new GameShape('oval', {
+                rX: 100,
+                rY: 100,
+                fill: 'green',
+                level: 1,
+                rotation: 0.2,
+                stroke: 'black',
+                lineWidth: 20
+            }),
+        ],
+        {clickable: true, name: '3-green', level: 1}
+    )
+    game.addElement(element3)
+
+    const element4 = new GameElement(350, 350,
+        [
+            new GameText('4', {level: 10}),
+            new GameShape('oval', {
+                rX: 100,
+                rY: 100,
+                fill: 'yellow',
+                level: 1,
+                rotation: 0.2,
+                stroke: 'black',
+                lineWidth: 20
+            }),
+        ],
+        {clickable: true, name: '4-yellow'}
+    )
+    game.addElement(element4)
+}
+pogs()
+
+// test display name of clicked object
+function displayClickedName() {
+    async function onClick(event) {
+        const mouse = game.getMousePos(event)
+        const clickedElement = await game.getElementAtPos(mouse)
+
+        console.log(`Clicked element with name: "${clickedElement.name}"`)
+    }
+
+    canvas.addEventListener('click',(ev => onClick(ev)))
+}
+displayClickedName()
+
+// test move clicked object to top
+function moveClickedToTop() {
+    async function onClick(event) {
+        const mouse = game.getMousePos(event)
+        const clickedElement = await game.getElementAtPos(mouse)
+
+        const highestLevel = Math.max(...game.elements.map(el => el.level))
+
+        clickedElement.level = highestLevel + 1
+        game.updateLevels()
+    }
+
+    canvas.addEventListener('mousedown',(ev => onClick(ev)))
+}
+moveClickedToTop()
+
+// uncomment this to test drawables
 // const test = new GameElement(300,300,
 //     [
-//         // new GameText('text',{level:10}),
-//         // new GameShape('rectangle',{width:100,height:50,fill:'red',level:0,rotation:0.5}),
-//         // new GameShape('rectangle',{width:100,height:100,stroke:'black',lineWidth:2,level:1}),
-//         // new GameText('level1',{level:1}),
-//         // new GameShape('oval',{rX:100,rY:100,fill:'red',level:1,rotation:0.2,stroke:'black',lineWidth:20}),
-//         // new GameShape('oval',{rX:50,rY:20,dx:200, dy:200,fill:'red',level:1,rotation:0.4}),
-//         // new GameShape('polygon',{name:'poly center',level:6, coords:[-100,-5,10,-10,30,30],fill:'red',rotation:0.3}),
-//         // new GameShape('polygon',{name:'poly right',level:6,dx:200, coords:[-100,-5,10,-10,30,30],fill:'red',rotation:0.3}),
-//         // new GameShape('line',{level:6, coords:[-100,-5,10,-10,30,30,200,-200],stroke:'black',lineWidth:50,}),
-//         // new GameShape('line',{level:7, coords:[-100,-5,10,-10,30,30,200,-200],stroke:'red',lineWidth:2,}),
-//         // new GameImage('frog','png',{name:'frog1',level:0,width:100,height:100,rotation:0.8}),
-//         // new GameImage('frog','png',{name:'frog2',level:0,dx:200,dy:200,width:200,height:100,rotation:-0.8}),
+//         new GameText('text',{level:10}),
+//         new GameShape('rectangle',{width:100,height:50,fill:'red',level:0,rotation:0.5}),
+//         new GameShape('rectangle',{width:100,height:100,stroke:'black',lineWidth:2,level:1}),
+//         new GameText('level1',{level:1}),
+//         new GameShape('oval',{rX:100,rY:100,fill:'red',level:1,rotation:0.2,stroke:'black',lineWidth:20}),
+//         new GameShape('oval',{rX:50,rY:20,dx:200, dy:200,fill:'red',level:1,rotation:0.4}),
+//         new GameShape('polygon',{name:'poly center',level:6, coords:[-100,-5,10,-10,30,30],fill:'red',rotation:0.3}),
+//         new GameShape('polygon',{name:'poly right',level:6,dx:200, coords:[-100,-5,10,-10,30,30],fill:'red',rotation:0.3}),
+//         new GameShape('line',{level:6, coords:[-100,-5,10,-10,30,30,200,-200],stroke:'black',lineWidth:50,}),
+//         new GameShape('line',{level:7, coords:[-100,-5,10,-10,30,30,200,-200],stroke:'red',lineWidth:2,}),
+//         new GameImage('frog','png',{name:'frog1',level:0,width:100,height:100,rotation:0.8}),
+//         new GameImage('frog','png',{name:'frog2',level:0,dx:200,dy:200,width:200,height:100,rotation:-0.8}),
 //         new GameGif('jump',{level:0,width:400,height:200,stagger:0}),
 //         new GameGif('colors',{level:-1,stagger:10,width:600,height:600}),
 //     ],
@@ -220,38 +362,4 @@ function button() {
 // )
 // game.addElement(test)
 
-// const element1 = new GameElement(250,250,
-//     [
-//             new GameText('1',{level:2}),
-//             new GameShape('oval',{rX:100,rY:100,fill:'red',level:1,rotation:0.2,stroke:'black',lineWidth:20}),
-//     ],
-//     {clickable:true,name:'1',level:10}
-// )
-// game.addElement(element1)
-//
-// const element2 = new GameElement(350,250,
-//     [
-//             new GameText('2',{level:10}),
-//             new GameShape('oval',{rX:100,rY:100,fill:'blue',level:1,rotation:0.2,stroke:'black',lineWidth:20}),
-//     ],
-//     {clickable:true,name:'2'}
-// )
-// game.addElement(element2)
-//
-// const element3 = new GameElement(250,350,
-//     [
-//             new GameText('3',{level:10}),
-//             new GameShape('oval',{rX:100,rY:100,fill:'green',level:1,rotation:0.2,stroke:'black',lineWidth:20}),
-//     ],
-//     {clickable:true,name:'3',level:1}
-// )
-// game.addElement(element3)
-//
-// const element4 = new GameElement(350,350,
-//     [
-//             new GameText('4',{level:10}),
-//             new GameShape('oval',{rX:100,rY:100,fill:'yellow',level:1,rotation:0.2,stroke:'black',lineWidth:20}),
-//     ],
-//     {clickable:true,name:'4'}
-// )
-// game.addElement(element4)
+
